a4c60523827dc44783ae6bf4eeca5e95c78b59ba
mmm25002500 <marryhan25648@gmail.com>
2026-03-03 19:25:12 +0800

    feat: add email and line

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,9 +5,15 @@ const Footer = () => {
     <footer className="bg-black py-8 mt-auto border-t border-white/10">
       <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-4 text-xs text-neutral-500">
         {/* 版權宣告 */}
-        <div>
-          © {new Date().getFullYear()} Go to future Development. All rights
-          reserved.
+        <div className="flex flex-col gap-2">
+          <div>
+            <p>電子信箱：<a href="mailto:marryhan25648@gmail.com" target="_blank" rel="noopener">marryhan25648@gmail.com</a></p>
+            <p>Line：@wayneop2500</p>
+          </div>
+          <p>
+            © {new Date().getFullYear()} Go to future Development. All rights
+            reserved.
+          </p>
         </div>
 
         <Link
